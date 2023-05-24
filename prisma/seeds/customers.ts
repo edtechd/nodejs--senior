6dcf51cb7f409bb3a6812ba92e525cd79cc121b4
Ed <support@edtech-corp.com>
2023-05-24 17:59:48 +0300

    Updated PostgreSQL schema: added role and activation code fields

--- prisma/seeds/customers.ts
+++ prisma/seeds/customers.ts
@@ -5,10 +5,14 @@ export const customers: Prisma.CustomerUpsertArgs['create'][] = [
     id: '9e391faf-64b2-4d4c-b879-463532920fd3',
     email: 'user@gmail.com',
     password: 'randow-password',
+    role: 'USER',
+    activationCode: null
   },
   {
     id: '9e391faf-64b2-4d4c-b879-463532920fd4',
     email: 'user2@gmail.com',
     password: 'randow-password',
+    role: 'ADMIN',
+    activationCode: null
   },
 ];
