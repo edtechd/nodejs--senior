bd442b04f51f7046e1f942eeb37b4bcf45475f45
Ed <support@edtech-corp.com>
2023-05-24 18:06:27 +0300

    Updated the customer module

---
+++ src/customer/customer.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards } from '@nestjs/common';
+import { CustomerDTO } from './dto/customer.dto';
+import { CustomerService } from './customer.service';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../decorators/role.decorator';
+
+@Controller('customers')
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Get(':id')
+  async getCustomerById(@Param('id') id: string): Promise<CustomerDTO> {
+    return this.customerService.getCustomerById(id);
+  }
+
+  @Get('email/:email')
+  async getCustomerByEmail(@Param('email') email: string): Promise<CustomerDTO> {
+    return this.customerService.getCustomerByEmail(email);
+  }
+
+  @Post()
+  @Roles('ADMIN')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+    async createCustomer(@Body() customerDto: CustomerDTO): Promise<CustomerDTO> {
+    return this.customerService.createCustomer(customerDto);
+  }
+
+  @Put(':id')
+  @Roles('ADMIN')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+    async updateCustomer(
+    @Param('id') id: string,
+    @Body() updatedCustomerDto: CustomerDTO,
+  ): Promise<CustomerDTO> {
+    return this.customerService.updateCustomer(id, updatedCustomerDto);
+  }
+
+  @Delete(':id')
+  @Roles('ADMIN')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async deleteCustomer(@Param('id') id: string): Promise<void> {
+    return this.customerService.deleteCustomer(id);
+  }
+}
--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
+import { CustomerController } from './customer.controller';
 import { PrismaService } from 'src/prisma.service';
 import { CustomerResolver } from './customer.resolver';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [ CustomerController ],
   providers: [CustomerService, PrismaService, CustomerResolver],
 })
 export class CustomerModule {}
